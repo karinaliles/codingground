c7ad308ba8dd51d79f54dc069066b4810661232c
karinaliles <karinaliles@users.noreply.github.com>
2017-08-20 12:10:07 -0400

    Created by Coding Ground Client

---
+++ KLilesCSCI207/Aug21/GradeBookTest.java
@@ -0,0 +1,17 @@
+/*Creating a GradeBook object and calling its displayMessage method
+Author: Prof. Liles
+Date: August 20, 2017
+*/
+
+public class GradeBookTest
+{
+    //main method begins program executino
+    public static void main(String[] args)
+    {
+        //create a GradeBook object and assign it to myGradeBook
+        GradeBook myGradeBook = new GradeBook();
+        
+        //call myGradeBook's displayMessage method
+        myGradeBook.displayMessage();
+    }//end main
+}//end class GradeBookTest
